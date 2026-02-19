80be5c472ef8f3ab9eed098b23adf36d6602baeb
Abinayapriya12 <priyakabinaya@gmail.com>
2026-02-19 10:23:36 +0530

    js

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+          //string
+/* let word = "madam";
+let letter = word.split("");
+let sec = letter.reverse();
+let value = sec.join("");
+console.log(value==word)   */
+                //number
+                
+let word = "1221";
+let name = word.toString();
+let letter = word.split("");
+let sec = letter.reverse();
+let value = sec.join("");
+console.log(value==word) 
+
