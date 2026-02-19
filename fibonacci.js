b9036ff8c4ca39fdf12820693087b562da027aee
Abinayapriya12 <priyakabinaya@gmail.com>
2026-02-19 10:28:12 +0530

    js

---
+++ fibonacci.js
@@ -0,0 +1,34 @@
+let n1= 0,
+n2= 1
+console.log(n1);
+console.log(n2);
+for(let i=3;i<=10;i++){
+    let n3 =n1+n2
+  console.log(n3);
+  n1=n2
+  n2=n3
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let n = 10; 
+let fib = [0, 1];
+
+for (let i = 2; i < n; i++) {
+  fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+console.log(fib);  */
