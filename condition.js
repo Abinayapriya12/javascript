2cdbeb4f771f102c498245acc3b083ba82782750
Abinayapriya12 <priyakabinaya@gmail.com>
2026-02-20 11:27:54 +0530

    js

---
+++ condition.js
@@ -0,0 +1,14 @@
+
+const n=12
+if(n>=16 && n<=20){ 
+console.log(" over cold");
+}
+else if(n>=21 && n<=25){ 
+console.log("cold");
+}
+else if(n>=26 && n<=30){ 
+console.log("normal");
+}
+else { 
+console.log("invalid!");
+}
