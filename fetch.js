1857d4a8cb9f3357bcbe274a7f81d5c96c5eee59
Abinayapriya12 <priyakabinaya@gmail.com>
2026-02-23 09:44:26 +0530

    js

---
+++ fetch.js
@@ -0,0 +1,28 @@
+ /*    async function fetchData(url){
+    try{
+        const data=await fetch(url)
+        const convertedData=await data.json()
+        console.log(convertedData)
+    }
+    catch(e){
+           console.log("error occur",e.message)
+    }
+}
+fetchData("https://jsonplaceholder.typicode.com/posts/1") 
+fetchData()  */
+
+ async function withIn(){
+    const response=await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    {if(!response)
+        throw("error")
+    }
+    return response.json()
+}
+ async function values(){
+    try{
+        const data = await withIn()
+        console.log("success",data)
+    }catch(e){console.log("error",e.message)
+
+    }
+ } values()
